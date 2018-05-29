4acfaf30c261eac8b44e65a616ff2ca1553ee21b
WeAreOneWeAreLegion <tercertrimestreproyecto@gmail.com>
2018-05-29 12:26:03 +0200

    Rooms
    Ya se puede hacer las habitaciones random.

--- Memory of Azov/Memory of Azov/Assets/Scripts/Objects Scripts/RoomScript.cs
+++ Memory of Azov/Memory of Azov/Assets/Scripts/Objects Scripts/RoomScript.cs
@@ -5,11 +5,80 @@ using UnityEngine;
 public class RoomScript : MonoBehaviour
 {
     #region Public Variables
+    [Header("\tGame Designers Variables")]
+    public int numOfDesiredGhosts;
+    public int numOfDesiredGems;
+    public List<EnemySO> enemiesData = new List<EnemySO>();
+
     [Header("\t    Own Script Variables")]
     [Tooltip("Lista de muros oscuros")]
     public List<GameObject> blackWalls = new List<GameObject>();
     [Tooltip("Lista de objetos que sueltan enemigos")]
-    public List<TransparentObject> objectsWithEnemies = new List<TransparentObject>();
+    #endregion
+
+    #region Private Variables
+    private int ghostIndex = 0;
+
+    private List<TransparentObject> objectsWithEnemies = new List<TransparentObject>();
+    private List<TransparentObject> objectsWithGems = new List<TransparentObject>();
+    private List<TransparentObject> allObjects = new List<TransparentObject>();
+    #endregion
+
+    private void Awake()
+    {
+        SetStartingRoom();
+    }
+
+    #region Set-Up Methods
+    private void SetStartingRoom()
+    {
+        List<TransparentObject> temporalObjects = new List<TransparentObject>();
+
+        foreach (TransparentObject t in GetComponentsInChildren<TransparentObject>())
+        {
+            t.spawnGhost = false;
+            t.spawnGem = false;
+            t.spawnAllGhosts = false;
+            allObjects.Add(t);
+            temporalObjects.Add(t);
+        }
+
+        objectsWithEnemies = new List<TransparentObject>();
+
+        int randomNum;
+
+        for (int i = 0; i < numOfDesiredGhosts; i++)
+        {
+            randomNum = Random.Range(0, temporalObjects.Count);
+
+            temporalObjects[randomNum].spawnGhost = true;
+            temporalObjects[randomNum].spawnAllGhosts = true;
+
+            if (ghostIndex < enemiesData.Count)
+            {
+                temporalObjects[randomNum].enemyData = enemiesData[ghostIndex];
+                ghostIndex++;
+            }
+            else
+            {
+                int randomData = Random.Range(0, enemiesData.Count);
+                temporalObjects[randomNum].enemyData = enemiesData[randomData];
+            }
+
+            objectsWithEnemies.Add(temporalObjects[randomNum]);
+            temporalObjects.RemoveAt(randomNum);
+        }
+
+        for (int i = 0; i < numOfDesiredGems; i++)
+        {
+            randomNum = Random.Range(0, temporalObjects.Count);
+
+            temporalObjects[randomNum].spawnGem = true;
+            temporalObjects.RemoveAt(randomNum);
+
+            objectsWithGems.Add(temporalObjects[randomNum]);
+        }
+    }
     #endregion
 
     #region Enemy Spawner Method
