a9f127be1c46111e2b47544b99ed90997c9c56ea
Jerometa <ocid@edu.tecnocampus.cat>
2018-06-08 20:30:15 +0200

    Balanceo

--- Memory of Azov/Memory of Azov/Assets/Scripts/Objects Scripts/DoorBell.cs
+++ Memory of Azov/Memory of Azov/Assets/Scripts/Objects Scripts/DoorBell.cs
@@ -45,6 +45,7 @@ public class DoorBell : MonoBehaviour {
         }
         else
         {
+			Debug.Log ("Door Open");
             myDoor.OpenByBell();
         }
 
--- Memory of Azov/Memory of Azov/Assets/Scripts/ScriptableObjects/EnemySO.cs
+++ Memory of Azov/Memory of Azov/Assets/Scripts/ScriptableObjects/EnemySO.cs
@@ -13,17 +13,17 @@ public class EnemySO : ScriptableObject
 
     [Header("Movement Variables")]
     [Tooltip("Velocidad a la que se mueve el personaje")]
-    [Range(0, 200)] public float speed = 100f;
+    [Range(0, 500)] public float speed = 100f;
     [Tooltip("Velocidad a la que rota el personaje")]
     [Range(0, 360)] public float rotationSpeed = 120f;
     [Tooltip("Velocidad a la que oscila el personaje")]
-    [Range(0, 200)] public float oscilationSpeed = 40;
+    [Range(0, 400)] public float oscilationSpeed = 40;
     [Tooltip("Cuantas veces ira mas rapido mientras lo enfocan")]
-    [Range(1, 5)] public float speedFactorWhenLightened = 2;
+    [Range(0, 10)] public float speedFactorWhenLightened = 2;
     [Tooltip("Cantidad de veces que se mueve de lado a lado por segundo")]
-    [Range(0, 5)] public float oscilationsPerSecond = 1f;
+    [Range(0, 10)] public float oscilationsPerSecond = 1f;
     [Tooltip("Longitud de la oscilacion de lado a lado (Linea verde)")]
-    [Range(1, 5)] public float oscilationAmplitude = 2;
+    [Range(0, 10)] public float oscilationAmplitude = 2;
     [Tooltip("Mira automaticamente al objectivo al qual esta dirigiendose si esta activado")]
     public bool immediateFacing;
     [Tooltip("Se mueve oscilatoriamente si esta activado")]
@@ -32,14 +32,14 @@ public class EnemySO : ScriptableObject
     [Header("Health Variables")]
     [Tooltip("Vida inicia del personaje")] public int initialHp = 100;
     [Tooltip("Tiempo que el personaje estara aturdido si lo aturden")]
-    [Range(1, 4)] public float timeStuned = 2f;
+    [Range(0, 10)] public float timeStuned = 2f;
 
     [Header("Attack Variables")]
     [Tooltip("Valor del ataque del personaje")]
-    public int ghostDamage = 20;
+	[Range(0, 100)] public int ghostDamage = 20;
     [Tooltip("Radio de ataque del personaje")]
-    [Range(0, 6)] public float attackRadius = 2f;
+    [Range(0, 20)] public float attackRadius = 2f;
     [Tooltip("Tiempo entre ataques")]
-    [Range(1, 4)] public float attackDelay = 2f;
+    [Range(0, 10)] public float attackDelay = 2f;
 
 }
