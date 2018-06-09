8ceeb4c2f8134a498468626201494ae864526dcb
Warcryer <stryder04.08@gmail.com>
2018-06-09 21:16:22 +0200

    Añadidas algunos sonidos. Nueva build de FMOD

---
+++ Memory of Azov/Memory of Azov/Assets/Scripts/Sounds/PlayRainSoundScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRainSoundScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SoundManager.Instance.AmbientSoundEnum(SoundManager.SoundRequestAmbient.A_Rain, this.gameObject.transform);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
